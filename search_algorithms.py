59ab4d8795d2efa49532e601d026de0184913721
Suhani <suhaniarora26@gmail.com>
2024-09-21 07:54:36 +0200

    added a*

--- search_algorithms.py
+++ search_algorithms.py
@@ -1,6 +1,6 @@
 from collections import deque
-
-
+from queue import PriorityQueue
+from routefinder import *
 
 ## We will append tuples (state, "action") in the search queue
 def breadth_first_search(startState, action_list, goal_test, use_closed_list=True) :
@@ -48,7 +48,7 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
         ## this is a (state, "action", depth) tuple
         next_state = search_queue.pop()
         states_generated += 1
-        if goal_test(next_state[0]):
+        if goal_test(next_state[0]) and (next_state[2] <= limit or limit == 0):
             print("Goal found")
             print(next_state)
             ptr = next_state[0]
@@ -65,7 +65,6 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
                     closed_list[s[0]] = True
             search_queue.extend([(item[0], item[1], next_state[2]+1) for item in successors])
     return None, states_generated
-## add iterative deepening search here
 
 def subproblem_search(startState, action_list):
     total_states = 0
@@ -92,3 +91,37 @@ def subproblem_search(startState, action_list):
     print("returnToCharger States: ", states_generated)
     
     return result, total_states
+
+
+def a_star(startState, heuristic_func, goal_test, use_closed_list=True) :
+    search_queue = PriorityQueue()
+    closed_list = {}
+    states_generated = 0
+
+    search_queue.put(startState)    
+    if use_closed_list :
+        closed_list[startState] = True
+        
+    while not search_queue.empty():
+        current_state = search_queue.get()
+        states_generated += 1
+        if goal_test(current_state):
+            return current_state, states_generated
+        successors = current_state.mars_graph.get_edges(current_state.location)
+        
+        for edge in successors:
+            next_state = map_state(edge.dest, current_state.mars_graph, current_state, current_state.g + 1)
+            next_state.h = heuristic_func(next_state)  # Heuristic for next state
+            next_state.f = next_state.g + next_state.h
+
+            if use_closed_list and next_state in closed_list:
+                continue
+
+            # Add the new state to the search queue
+            search_queue.put(next_state)
+
+            if use_closed_list:
+                closed_list[next_state] = True
+    
+    print(f"Search completed. Total states generated: {states_generated}")
+    return None, states_generated
